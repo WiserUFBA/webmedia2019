0895889cdb894a66ef44ceebc3cf3d3b51c5cf97
cleberlira <cleberlira@cleberlira-Inspiron-5566>
2019-10-23 11:33:51 -0300

    circuit breaker for Webmedia 2019

---
+++ NetBeansProjects/circuit-breaker-example-webmedia/src/test/java/br/ufba/dcc/wiser/circuitbreaker/provider/Provider.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufba.dcc.wiser.circuitbreaker.provider;
+
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+
+/**
+ *
+ * @author cleberlira
+ */
+public class Provider extends AbstractVerticle {
+    
+    
+    @Override
+    public void start() {
+        Router router = Router.router(vertx);
+        router.get("/").handler(this::webmedia);
+        router.get("/:name").handler(this::webmedia);
+        vertx.createHttpServer()
+            .requestHandler(router::accept)
+            .listen(8080);
+    }
+
+    private void webmedia(RoutingContext rc) {
+        String message = "hello";
+        if (rc.pathParam("name") != null) {
+            message += " " + rc.pathParam("name");
+        }
+        JsonObject json = new JsonObject()
+            .put("mensagem", message)
+            .put("Fornecido por: ", System.getenv("HOSTNAME"));
+        rc.response()
+            .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+            .end(json.encode());
+    }
+}
